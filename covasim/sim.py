eb5e059ee2143a52368aa992c27af2e3ddadb24e
Maryam <mary.elf97@gmail.com>
2026-08-12 14:46:29 -0700

    store sheddings for each time step for each agent

--- covasim/sim.py
+++ covasim/sim.py
@@ -641,7 +641,11 @@ class Sim(cvb.BaseSim):
         date_rec = people.date_recovered
         date_dead = people.date_dead
         viral_load = cvu.compute_viral_load(t, date_inf, date_rec, date_dead, frac_time, load_ratio, high_cap)
-        viral_shedding = cvu.compute_viral_shedding(date_inf, self.pars["n_days"])
+        viral_shedding = cvu.compute_viral_shedding(
+            t=self.t, 
+            date_infectious=date_inf, 
+            shedding_lookup=self.shedding_lookup
+        )
         self.people.viral_load     = viral_load
         self.people.viral_shedding = viral_shedding
         self.results['viral_load_hist'][:, self.t]     = viral_load
